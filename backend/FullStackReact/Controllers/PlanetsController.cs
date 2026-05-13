f6400b8499797add56ccdd701f5a6fbf9f047eb5
Katri Lotamõis <katrilotamois@outlook.com>
2026-05-13 13:17:38 +0300

    create controller method for adding a new planet

--- backend/FullStackReact/Controllers/PlanetsController.cs
+++ backend/FullStackReact/Controllers/PlanetsController.cs
@@ -1,4 +1,5 @@
 using FullStackReact.DbConfig;
+using FullStackReact.Entities;
 using FullStackReact.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,8 @@ public class PlanetsController : ControllerBase
     {
         _dbContext = dbContext;
     }
+    
+    [HttpGet]
     public IActionResult GetAllPlanets()
     {
         var result = _dbContext.Planets
@@ -27,5 +30,29 @@ public class PlanetsController : ControllerBase
             });
         return Ok(result);
     }
-    
+
+    [HttpPost("new")]
+    public async Task<IActionResult> AddNewPlanet([FromBody] PlanetsListViewModel planet)
+    {
+        try
+        {
+            var newPlanet = new Planet
+            {
+                Name = planet.Name,
+                Description = planet.Description,
+                Type = planet.Type,
+                Mass = planet.Mass
+            };
+
+            _dbContext.Add(newPlanet);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(new {message = "Adding planet was successful!"});
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.StackTrace);
+            return BadRequest("Could not save planet to db: "  + e.Message);
+        }
+    }
 }
